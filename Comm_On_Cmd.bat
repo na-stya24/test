c8d74e8672f59bd15613f1fada529a7ca3550dfc
Anastasia Tkach <nst@gmail.com>
2025-10-24 20:23:41 +0300

    uodated

--- Comm_On_Cmd.bat
+++ Comm_On_Cmd.bat
@@ -1,3 +1,7 @@
+<<<<<<< HEAD
+=======
+#command
+>>>>>>> d2d0b5e (uodated)
 C:\Users\nastk>echo "Hello desktop" > "%HOMEPATH%\OneDrive\Desktop\desktopcmd.txt"
 
 C:\Users\nastk>echo Hello desktop > "%USERPROFILE%\OneDrive\Desktop\desktopcmd.txt"
